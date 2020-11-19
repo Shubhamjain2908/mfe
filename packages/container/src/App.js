a36940bf2b58a7eb880b4ad947806219d0bc984b
Shubham Jain <shubhamjain2908@gmail.com>
2020-11-19 14:44:00 +0530

    bug fix of switch

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,6 @@
-import { Switch } from '@material-ui/core';
 import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AuthApp from './components/AuthApp';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
